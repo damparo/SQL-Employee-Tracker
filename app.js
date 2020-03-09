b181dc096ece4c8d3dd2f5cec031e6b4ae58bd5c
Dennis Amparo <dennisamparo11@gmail.com>
2020-03-08 20:34:13 -0700

    addfuncdone

--- app.js
+++ app.js
@@ -14,3 +14,101 @@ var connection = mysql.createConnection({
   password: "",
   database: "company_db"
 });
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  addDRE()
+  // queryEmpinfo();
+});
+
+function addDRE () {
+  inquirer
+    .prompt ([
+      {
+        type: "list",
+        name: "updateDRE",
+        message: "Would you like to add [department], [role], or [employee]?",
+        choices: ["department", "role", "employee", "exit"]
+      }
+    ]).then(function(response) {
+      switch (response.updateDRE) {
+        case "department":
+          addDEPT();
+          break;
+        case "role":
+          addROLE();
+          break;
+        case "employee":
+          addEMP();
+          break;
+    
+      }
+    });
+}
+
+function addDEPT () {
+  var query = connection.query(
+    "INSERT INTO department SET ?",
+  {
+    department_name: "human resources",
+
+  },
+  function(err, res) {
+    if (err) throw err;
+    console.log(res.affectedRows);
+    addDRE();
+  }
+  );
+  console.log(query.sql);
+  // console.log(table);
+}
+
+function addROLE () {
+  var query = connection.query(
+    "INSERT INTO _role SET ?",
+  {
+    title: "chemical engineer",
+    salary: "35",
+    department_id:"007"
+
+  },
+  function(err, res) {
+    if (err) throw err;
+    console.log(res.affectedRows);
+    addDRE();
+  }
+  );
+  console.log(query.sql);
+  // console.log(table);
+}
+
+function addEMP () {
+  var query = connection.query(
+    "INSERT INTO _employee SET ?",
+  {
+    first_name: "james",
+    last_name: "James",
+    role_id:"998",
+    manager_id: "111"
+  },
+  function(err, res) {
+    if (err) throw err;
+    console.log(res.affectedRows);
+    addDRE();
+  }
+  );
+  // console.log(query.sql);
+  console.table(res);
+}
+
+// function queryEmpinfo() {
+//   // figure out query inside workbench then copy to js
+//   connection.query("SELECT * FROM _employee", function(err, res) {
+//     if (err) throw err;
+//     // console.log(res);
+//     console.table(res);
+//     // connnection.end kills connection after response
+//     connection.end();
+//   });
+// }
